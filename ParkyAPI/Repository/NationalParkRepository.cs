25efcd3162d63913f712a1ae7140e88c49f2f753
Ratakarn.Bunt <Ratakarn.Bunt@THBPONBH007266>
2020-10-29 18:57:08 +0700

    Add project files.

---
+++ ParkyAPI/Repository/NationalParkRepository.cs
@@ -0,0 +1,62 @@
+﻿using ParkyAPI.Data;
+using ParkyAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Repository
+{
+    public class NationalParkRepository : INationalParkRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public NationalParkRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public bool CreateNationalPark(NationalPark nationalPark)
+        {
+            _db.NationalParks.Add(nationalPark);
+            return Save();
+        }
+
+        public bool DeleteNAtionalPark(NationalPark nationalPark)
+        {
+            _db.NationalParks.Remove(nationalPark);
+            return Save();
+        }
+
+        public NationalPark GetNationalPark(int nationalParkId)
+        {
+            return _db.NationalParks.FirstOrDefault(x => x.Id == nationalParkId);
+        }
+
+        public ICollection<NationalPark> GetNationalParks()
+        {
+            return _db.NationalParks.OrderBy(x => x.Name).ToList();
+        }
+
+        public bool NationalParkExists(string name)
+        {
+            bool value = _db.NationalParks.Any(x => x.Name.ToLower().Trim() == name.ToLower().Trim());
+            return value;
+        }
+
+        public bool NationalParkExists(int id)
+        {
+            return _db.NationalParks.Any(x => x.Id == id);
+        }
+
+        public bool Save()
+        {
+            return _db.SaveChanges() >= 0 ? true : false;
+        }
+
+        public bool UpdateNationalPark(NationalPark nationalPark)
+        {
+            _db.NationalParks.Update(nationalPark);
+            return Save();
+        }
+    }
+}
